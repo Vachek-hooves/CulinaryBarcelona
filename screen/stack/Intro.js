b34b2e8bb24484a4c9329a09123cd16cda344f19
Vachek-hooves <vachek0602@gmail.com>
2025-02-10 09:48:49 +0200

    updates

--- screen/stack/Intro.js
+++ screen/stack/Intro.js
@@ -1,34 +1,38 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { useEffect } from 'react'
+import {StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
+import {useEffect} from 'react';
 
 const Intro = ({navigation}) => {
-    useEffect(() => {
-        setTimeout(() => {
-            navigation.navigate('OnBoarding')
-        }, 1000)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('OnBoarding');
+    }, 1000);
+  }, []);
   return (
-    <View style={styles.container}>
-      <Image 
+    //   <View style={styles.container}>
+    <ImageBackground
+      source={require('../../assets/image/bg/bg.png')}
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Image
         source={require('../../assets/image/logo/logo.png')}
         style={styles.logo}
         resizeMode="contain"
       />
-    </View>
-  )
-}
+    </ImageBackground>
+    // </View>
+  );
+};
 
-export default Intro
+export default Intro;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    // backgroundColor: '#000000',
     justifyContent: 'center',
     alignItems: 'center',
   },
   logo: {
     width: '100%',
     height: 100,
-  }
-})
+  },
+});
